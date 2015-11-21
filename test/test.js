9fdae04dcd28c70bf913f8d8cd9bb115fb4c700d
Andi Sidwell <andi@takkaria.org>
2015-11-21 20:37:15 +0000

    Move test data into .json files next to the templates

--- test/test.js
+++ test/test.js
@@ -6,86 +6,38 @@ var path = require('path');
 var Pend = require('pend');
 var fs = require('fs');
 
+// Index of tests
+// testName: what it should do
 var testMap = {
-  "simple_vars": {
-    hello: "hello"
-  },
-  "two_vars_content": {
-    one: "one",
-    two: "two"
-  },
-  "for_loop": {
-    scalar: "scalar",
-    scalar2: "scalar2",
-    xyz: [ "abcd" ],
-    lalala: "lalala"
-  },
-  "plays": {
-    subject: "subject",
-    emailPreferencesUrl: "emailPreferencesUrl",
-    userImgUrl: "userImgUrl",
-    playCount: "playCount",
-    commentCount: "commentCount",
-    voteCount: "voteCount",
-    facebookShareUrl: "facebookShareUrl",
-    twitterShareUrl: "twitterShareUrl",
-    userName: "userName",
-    opportunityListenUrl: "opportunityListenUrl",
-    submissionStatsUrl: "submissionStatsUrl"
-  },
-  "if_statement": {
-    one: "one",
-    two: "two",
-    three: "three",
-    four: "four",
-    five: "five",
-    foo: "foo",
-    derp: "derp"
-  },
-  "comments": {
-    twenty: "twenty",
-    ten: "ten",
-    eleven: "eleven",
-    baseOne: "baseOne",
-    baseTwo: "baseTwo"
-  },
-  "complex_variable": {
-    scalar: "scalar",
-    one: {
-      two: {
-        three: "three",
-        four: "four"
-      },
-      five: {
-        four: "four"
-      },
-      six: "six"
-    },
-    foo: {
-      bar: "bar",
-      arr: [{
-        prop: [{
-          prop2: "prop2"
-        }]
-      }]
-    },
-    xyz: [[
-      {
-        one: "one",
-        two: "two"
-      }
-    ]],
-    lalala: "lalala",
-    la2: "la2"
-  }
+  'simple_vars': 'should do simple variable substitution',
+  'two_vars_content': 'two_vars_content',
+  'for_loop': 'for_loop',
+  'if_statement': 'if_statement',
+  'comments': 'comments',
+  'complex_variable': 'complex_variable',
+  'plays': 'plays'
 };
+
+// Attempt to
+function attemptReadFile(path, encoding, cb) {
+  fs.readFile(path, encoding, function(err, text) {
+    if (err && err.code === 'ENOENT') {
+      err = null;
+    }
+
+    cb(err, text);
+  });
+}
 
 describe("swig-email-templates", function() {
   var render;
+  var templatePath = path.join(__dirname, "templates");
+
   before(function(cb) {
     var options = {
-      root: path.join(__dirname, "templates")
+      root: templatePath
     };
+
     emailTemplates(options, function(err, renderFn) {
       if (err) {
         cb(err);
@@ -96,47 +48,56 @@ describe("swig-email-templates", function() {
     });
   });
 
-  for (var templateName in testMap) {
-    it(templateName, createIt(templateName, testMap[templateName]));
+  for (var testName in testMap) {
+    var should = testMap[testName];
+    it(should + ' (' + testName + ')', createIt(testName));
   }
   
-  function createIt(templateName, context) {
+  function createIt(testName) {
     return function(cb) {
-      var pend = new Pend();
+      var testPath = path.join(templatePath, testName);
+
       var expectedHtml, actualHtml;
       var expectedText, actualText;
+
+      var pend = new Pend();
+
+      // Read the JSON file & render the HTML
       pend.go(function(cb) {
-        render(templateName + '.html', context, rewrite, function(err, html, text) {
-          actualHtml = html;
-          actualText = text;
+        attemptReadFile(testPath + '.json', 'utf8', function(err, data) {
+          var context = {};
+
+          if (err) return cb(err);
+          if (data) context = JSON.parse(data);
+
+          render(testName + '.html', context, rewrite, function(err, html, text) {
+            actualHtml = html;
+            actualText = text;
+            cb(err);
+          });
+        });
+      });
+
+      // Load the expected HTML
+      pend.go(function(cb) {
+        attemptReadFile(testPath + '.out.html', 'utf8', function(err, html) {
+          expectedHtml = html;
           cb(err);
         });
       });
+
+      // Try loading the expected text (may not exist)
       pend.go(function(cb) {
-        var filename = path.join(__dirname, "templates", templateName + ".out.html");
-        fs.readFile(filename, 'utf8', function(err, val) {
-          expectedHtml = val;
+        attemptReadFile(testPath + '.out.txt', 'utf8', function(err, text) {
+          expectedText = text;
           cb(err);
         });
       });
-      pend.go(function(cb) {
-        var filename = path.join(__dirname, "templates", templateName + ".out.txt");
-        fs.readFile(filename, 'utf8', function(err, val) {
-          if (err) {
-            if (err.code === 'ENOENT') {
-              expectedText = null;
-              cb();
-            } else {
-              cb(err);
-            }
-          } else {
-            expectedText = val;
-            cb();
-          }
-        });
-      });
+
+      // And when they're all done...
       pend.wait(function(err) {
         if (err) return cb(err);
+
         assert.strictEqual(actualHtml.trim(), expectedHtml.trim());
         if (expectedText) {
           assert.strictEqual(actualText.trim(), expectedText.trim());
